4ee63124e14411e3c8d82abc3e81d178e80a8fe0
eskalona17 <eskalona_17@hotmail.com>
2020-05-14 12:15:48 +0200

    conditional loading component done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { useState, useEffect } from "react";
 import Pregunta from "./components/Pregunta";
-import Formulario from './components/Formulario'
+import Formulario from "./components/Formulario";
 
 function App() {
   //definir el state
   const [presupuesto, guardarPresupuesto] = useState(0);
   const [restante, guardarRestante] = useState(0);
+  const [mostrarPregunta, actualizarPregunta] = useState(true);
   return (
     <div className="container">
       <header>
         <h1>Gasto semanal</h1>
         <div className="contenido-principal contenido">
-          <Pregunta
-            guardarPresupuesto={guardarPresupuesto}
-            guardarRestante={guardarRestante}
-          />
-          <div className="row">
-            <div className="one-half column">
-              <Formulario />
+          {mostrarPregunta ? (
+            <Pregunta
+              guardarPresupuesto={guardarPresupuesto}
+              guardarRestante={guardarRestante}
+              actualizarPregunta={actualizarPregunta}
+            />
+          ) : (
+            <div className="row">
+              <div className="one-half column">
+                <Formulario />
+              </div>
+              <div className="one-half column">2</div>
             </div>
-            <div className="one-half column">
-              2
-            </div>
-          </div>
+          )}
         </div>
       </header>
     </div>
